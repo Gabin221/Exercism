da1e9e1e3889d60b3699687abfc95b3af1e9aef6
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-20 16:46:02 +0100

    Update

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -3,39 +3,15 @@
 nbr=$#
 if [ $nbr -eq 1 ]; 
 then 
-	input=$1
-    position=$(expr index "$input" "*")
-
-    if [[ $position -ne 0 ]]; 
-    then
-        split1=${input::position-1}${input:position:${#input}}
-        listeMots=$(echo $split1 | tr "-" "\n" | tr "_" "\n")
-        accronyme=""
-        taille=${#listeMots}+1
-
-        for ip in $listeMots
-        do
-            mot=${ip^}
-            initiale=${mot::1}
-            accronyme=$accronyme"$initiale"
-        done
-
-        echo "$accronyme"
-    else
-        split1=${input::position-1}${input:position:${#input}}
-        listeMots=$(echo $input | tr "-" "\n" | tr "_" "\n")
-        accronyme=""
-        taille=${#listeMots}+1
-
-        for ip in $listeMots
-        do
-            mot=${ip^}
-            initiale=${mot::1}
-            accronyme=$accronyme"$initiale"
-        done
-
-        echo "$accronyme"
-    fi
+    input=$1
+	IFS='- _*'
+    acronym=""
+    for word in $input;
+    do
+        letter="${word:0:1}"
+        acronym+=${letter^^}
+    done
+    echo "$acronym"
 else 
     echo "Usage: acronym.sh <person>"
     exit 1
