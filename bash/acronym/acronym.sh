b31bdf0c3580bb653c9262a4f95cfeb7ac2889cc
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-20 12:42:59 +0100

    Update acronym.sh

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-echo "What is your input ?"
-read varname
-echo "It's nice to meet you $varname"
+# echo "What is your input ?"
+# read varname
+input="Liquid-crystal display"
+arrIN=(${input//blank/ })
+taille=${#arrIN}+1
+accronyme=""
+
+for (( i=0; i<=taille; i++ ))
+do
+    mot=${arrIN[i]^}
+    accronyme=$accronyme"${mot::1}"
+done
+
+echo "$accronyme"
