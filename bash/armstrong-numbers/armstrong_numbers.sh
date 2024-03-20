bf80bca192767cba6fc0a8a52e892e6bce3cf5ec
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-20 18:23:11 +0100

    Update armstrong_numbers.sh

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+nbr=$#
+if [ $nbr -eq 1 ]; 
+then 
+    input=$1
+    string=""
+	for i in $input;
+    do
+        echo $i
+    done
+else 
+    echo "Usage: acronym.sh <person>"
+    exit 1
+fi
