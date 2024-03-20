cc9ce991fb727dca872e19e77a9a160d31080198
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-20 15:58:47 +0100

    Update acronym.sh

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -5,7 +5,7 @@ if [ $nbr -eq 1 ];
 then 
 	input=$1
     ascii_value=$(od -An -t u1 <<< "\*")
-    listeMots=$(echo $input | tr "-" "\n" | tr "_" "\n" | tr "\42" "\n")
+    listeMots=$(echo $input | tr "-" "\n" | tr "_" "\n" | tr \* "\n")
     accronyme=""
     taille=${#listeMots}+1
 
