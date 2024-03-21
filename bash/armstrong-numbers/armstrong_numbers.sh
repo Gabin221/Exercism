236dbb71a53ce1518ef16aa4dd65090e7bf01f9a
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-21 13:59:14 +0100

    Update

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -3,22 +3,11 @@
 nbr=$#
 if [ $nbr -eq 1 ]; 
 then 
-    x=$1
-    y=$x
-    b=10
-    digit_count=0
-    while [ $y > 0 ]:
+    nbr=$1
+    for i in ${#nbr};
     do
-        digit_count=$((digit_count+1))
-        y=$((y/b))
+	echo $i
     done
-    total=0
-    while [ $x > 0 ]:
-    do
-        total=$((total+(x%b)**digit_count))
-        x=$((x/b))
-    done
-    echo "total=$total"
 else 
     echo "Usage: acronym.sh <person>"
     exit 1
