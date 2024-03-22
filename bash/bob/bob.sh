a54ad5dc9a8598c08b3212f53b0f5a922902b84f
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-22 12:08:22 +0100

    Update bob.sh

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -4,34 +4,21 @@ nbr=$#
 if [ $nbr -eq 1 ]; 
 then 
     question=$1
-    something=0
-    # for lettre in $(echo "$question");
-    # do
     if [[ $question == *"?"* ]];
     then
         echo "Sure."
-        something=$((something+1))
-    fi
-    if [[ "$question" == "${question^^}" ]];
+    elif [[ "$question" == "${question^^}" ]];
     then
         echo "Whoa, chill out!"
-        something=$((something+1))
-    fi
-    if [[ $question == *"?"* ]];
+    elif [[ $question == *"?"* ]];
     then
         echo "Calm down, I know what I'm doing!"
-        something=$((something+1))
-    fi
-    if [[ $lettre == *"$(echo "$question" | grep -q " "*; echo $?)"* ]];
+    elif [[ ${question} =~ ^[[:space:]]*$ ]];
     then
         echo "Fine. Be that way!"
-        something=$((something+1))
-    fi
-    if [[ $something -eq 0 ]];
-    then
+    else
         echo "Whatever."
     fi
-    # done
 else 
     echo "Usage: acronym.sh <person>"
     exit 1
