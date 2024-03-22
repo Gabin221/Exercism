08fd55f0d1818db4d859b4ebf21dc8898987bfcc
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-22 12:52:34 +0100

    Update

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -4,14 +4,33 @@ nbr=$#
 if [ $nbr -eq 1 ]; 
 then 
     input=$1
-	IFS='- _*'
-    acronym=""
-    for word in $input;
+    score=0
+    for (( i=0; i<${#input}; i++ ));
     do
-        letter="${word:0:1}"
-        acronym+=${letter^^}
+        lettre=${input:$i:1}
+        if [[ "aeioulnrst" == *"$lettre"* ]] || [[ "AEIOULNRST" == *"$lettre"* ]];
+        then
+            score=$((score+1))
+        elif [[ "dg" == *"$lettre"* ]] || [[ "DG" == *"$lettre"* ]];
+        then
+            score=$((score+2))
+        elif [[ "bcmp" == *"$lettre"* ]] || [[ "BCMP" == *"$lettre"* ]];
+        then
+            score=$((score+3))
+        elif [[ "fhvwy" == *"$lettre"* ]] || [[ "FHVWY" == *"$lettre"* ]];
+        then
+            score=$((score+4))
+        elif [[ "k" == *"$lettre"* ]] || [[ "K" == *"$lettre"* ]];
+        then
+            score=$((score+5))
+        elif [[ "jx" == *"$lettre"* ]] || [[ "JX" == *"$lettre"* ]];
+        then
+            score=$((score+8))
+        else
+            score=$((score+10))
+        fi
     done
-    echo "$acronym"
+    echo $score
 else 
     echo "Usage: acronym.sh <person>"
     exit 1
