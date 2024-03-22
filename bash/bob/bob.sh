cc074da1435d161517a1c61176d1a5e4522ca2e0
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-22 12:30:24 +0100

    Update

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
-nbr=$#
-if [ $nbr -eq 1 ]; 
-then 
-    question=$1
-    if [[ $question == *"?"* ]];
-    then
-        echo "Sure."
-    elif [[ "$question" == "${question^^}" ]];
-    then
-        echo "Whoa, chill out!"
-    elif [[ $question == *"?"* ]];
-    then
-        echo "Calm down, I know what I'm doing!"
-    elif [[ ${question} =~ ^[[:space:]]*$ ]];
-    then
-        echo "Fine. Be that way!"
-    else
-        echo "Whatever."
-    fi
-else 
-    echo "Usage: acronym.sh <person>"
-    exit 1
+
+question=$1
+if [[ ${question} =~ ^[[:space:]]*$ ]];
+then
+    echo "Fine. Be that way!"
+elif [[ ${question} =~ ^[^[:lower:]]+$ && ${question} =~ [[:upper:]] ]] && [[ ${question} =~ \?\ *$ ]];
+then
+    echo "Calm down, I know what I'm doing!"
+elif [[ ${question} =~ ^[^[:lower:]]+$ && ${question} =~ [[:upper:]] ]];
+then
+    echo "Whoa, chill out!"
+elif [[ ${question} =~ \?\ *$ ]];
+then
+    echo "Sure."
+else
+    echo "Whatever."
 fi
