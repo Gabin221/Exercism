af2c8307c736896ee6ef736afe65eec68d5188f7
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-26 14:00:36 +0100

    Update

--- cpp/hello-world/hello_world.cpp
+++ cpp/hello-world/hello_world.cpp
@@ -1,4 +1,5 @@
 #include "hello_world.h"
+#include <iostream>
 
 using namespace std;
 
@@ -7,7 +8,12 @@ namespace hello_world
 
 string hello()
 {
-    return "Goodbye, Mars!";
+    return "Hello, World!";
 }
 
+}
+
+int main () {
+    cout << hello_world::hello() << endl;
+    return 0;
 }
