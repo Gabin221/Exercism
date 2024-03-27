e2da18050a6dcca06f35075560f7550b0ab7f25f
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-27 15:43:43 +0100

    Update

--- cpp/last-will/last_will.cpp
+++ cpp/last-will/last_will.cpp
@@ -43,3 +43,14 @@ namespace garcia {
 }
 
 // Enter your code below
+namespace estate_executor {
+    int assemble_account_number(int secret_modifier) {
+        return zhang::bank_number_part(secret_modifier) + khan::bank_number_part(secret_modifier) + garcia::bank_number_part(secret_modifier);
+    }
+
+    int assemble_code() {
+        int red = zhang::red::code_fragment() + khan::red::code_fragment() + garcia::red::code_fragment();
+        int blue = zhang::blue::code_fragment() + khan::blue::code_fragment() + garcia::blue::code_fragment();
+        return red*blue;
+    }
+}
