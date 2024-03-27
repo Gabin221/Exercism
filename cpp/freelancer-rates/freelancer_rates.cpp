3953e2bb002a4ae42a2074a0aa7343002ce47b9c
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-27 16:15:03 +0100

    Update

--- cpp/freelancer-rates/freelancer_rates.cpp
+++ cpp/freelancer-rates/freelancer_rates.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+using namespace std;
+
 // INFO: Headers from the standard library should be inserted at the top via
 // #include <LIBRARY_NAME>
 
@@ -5,13 +8,13 @@
 double daily_rate(double hourly_rate) {
     // TODO: Implement a function to calculate the daily rate given an hourly
     // rate
-    return 0.0;
+    return hourly_rate*8.0;
 }
 
 // apply_discount calculates the price after a discount
 double apply_discount(double before_discount, double discount) {
     // TODO: Implement a function to calculate the price after a discount.
-    return 0.0;
+    return before_discount - (before_discount*discount/100.0);
 }
 
 // monthly_rate calculates the monthly rate, given an hourly rate and a discount
@@ -19,7 +22,8 @@ double apply_discount(double before_discount, double discount) {
 int monthly_rate(double hourly_rate, double discount) {
     // TODO: Implement a function to calculate the monthly rate, and apply a
     // discount.
-    return 0;
+    double monthly_rate = apply_discount(22.0*daily_rate(hourly_rate), discount);
+    return monthly_rate;
 }
 
 // days_in_budget calculates the number of workdays given a budget, hourly rate,
@@ -28,5 +32,10 @@ int monthly_rate(double hourly_rate, double discount) {
 int days_in_budget(int budget, double hourly_rate, double discount) {
     // TODO: Implement a function that takes a budget, an hourly rate, and a
     // discount, and calculates how many complete days of work that covers.
+    return 0;
+}
+
+int main(){
+    cout << monthly_rate(77, 10.5) << endl;
     return 0;
 }
