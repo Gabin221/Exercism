e6a83738fabfca72ba342bf234a64e663478ccf2
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-28 10:39:23 +0100

    Update

--- cpp/pacman-rules/pacman_rules.cpp
+++ cpp/pacman-rules/pacman_rules.cpp
@@ -3,7 +3,11 @@
 // and is touching a ghost.
 bool can_eat_ghost(bool power_pellet_active, bool touching_ghost) {
     // TODO: Please implement the can_eat_ghost function
-    return false;
+    if (power_pellet_active && touching_ghost) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // score returns a boolean value if Pac-Man scored.
@@ -11,7 +15,11 @@ bool can_eat_ghost(bool power_pellet_active, bool touching_ghost) {
 // dot.
 bool scored(bool touching_power_pellet, bool touching_dot) {
     // TODO: Please implement the scored function
-    return false;
+    if (touching_power_pellet || touching_dot) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // lost returns a boolean value if Pac-Man loses.
@@ -19,14 +27,21 @@ bool scored(bool touching_power_pellet, bool touching_dot) {
 // does not have a power pellet active.
 bool lost(bool power_pellet_active, bool touching_ghost) {
     // TODO: Please implement the lost function
-    return false;
+    if (power_pellet_active == false && touching_ghost) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // won returns a boolean value if Pac-Man wins.
 // The function should return true if Pac-Man
 // has eaten all of the dots and has not lost
-bool won(bool has_eaten_all_dots, bool power_pellet_active,
-         bool touching_ghost) {
+bool won(bool has_eaten_all_dots, bool power_pellet_active, bool touching_ghost) {
     // TODO: Please implement the won function
-    return false;
+    if (lost(power_pellet_active, touching_ghost) == false && has_eaten_all_dots) {
+        return true;
+    } else {
+        return false;
+    }
 }
