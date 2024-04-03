c50aa6293c6fc3e45d03b6e5ed4268065b4a6f64
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-04-03 22:09:35 +0200

    Update grains.sh

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+nbr=$#
+if [ $nbr -eq 1 ]; 
+then 
+    input=$1
+    factorielleComplet=0
+    factorielleInput=$((2**(input-1)))
+
+    for (( i=1; i<=64; i++ ));
+    do
+        factorielleComplet=$((factorielleComplet+2**i))
+        echo "${i}: ${factorielleComplet}"
+    done
+
+    echo $factorielleComplet
+    echo $factorielleInput
+else 
+    echo "Usage: grains.sh <person>"
+    exit 1
+fi
