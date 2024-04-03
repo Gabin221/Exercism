dfd3b603f383fc44b03069d17758dd6be6efb3c8
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-04-03 15:34:40 +0200

    Update

--- cpp/making-the-grade/making_the_grade.cpp
+++ cpp/making-the-grade/making_the_grade.cpp
@@ -1,40 +1,54 @@
 #include <array>
 #include <string>
 #include <vector>
+#include <iostream>
+
+using namespace std;
 
 // Round down all provided student scores.
-std::vector<int> round_down_scores(std::vector<double> student_scores) {
+vector<int> round_down_scores(vector<double> student_scores) {
     // TODO: Implement round_down_scores
-    return {};
+    vector<int> scores = {};
+
+    for (unsigned int i = 0; i < student_scores.size(); i++) {
+            scores.push_back((int)student_scores[i]);
+    }
+
+    return scores;
 }
 
 
 // Count the number of failing students out of the group provided.
-int count_failed_students(std::vector<int> student_scores) {
+int count_failed_students(vector<int> student_scores) {
     // TODO: Implement count_failed_students
     return 0;
 }
 
 // Determine how many of the provided student scores were 'the best' based on the provided threshold.
-std::vector<int> above_threshold(std::vector<int> student_scores, int threshold) {
+vector<int> above_threshold(vector<int> student_scores, int threshold) {
     // TODO: Implement above_threshold
     return {};
 }
 
 // Create a list of grade thresholds based on the provided highest grade.
-std::array<int, 4> letter_grades(int highest_score) {
+array<int, 4> letter_grades(int highest_score) {
     // TODO: Implement letter_grades
     return {};
 }
 
 // Organize the student's rank, name, and grade information in ascending order.
-std::vector<std::string> student_ranking(std::vector<int> student_scores, std::vector<std::string> student_names) {
+vector<string> student_ranking(vector<int> student_scores, vector<string> student_names) {
     // TODO: Implement student_ranking
     return {};
 }
 
 // Create a string that contains the name of the first student to make a perfect score on the exam.
-std::string perfect_score(std::vector<int> student_scores, std::vector<std::string> student_names) {
+string perfect_score(vector<int> student_scores, vector<string> student_names) {
     // TODO: Implement perfect_score
     return "";
+}
+
+int main() {
+    round_down_scores({90.33, 40.5, 55.44, 70.05, 30.55, 25.45, 80.45, 95.3, 38.7, 40.3});
+    return 0;
 }
