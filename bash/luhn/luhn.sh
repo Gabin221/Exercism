5954fdeeeb6a69144b688a54312a8b17757aadc5
Gabin221 <serrurot19@gmail.com>
2024-04-04 15:31:32 +0200

    Update

--- bash/luhn/luhn.sh
+++ bash/luhn/luhn.sh
@@ -1,49 +1,30 @@
 #!/usr/bin/env bash
 
-nbr=$#
+input=$1
 
-if [ $nbr -eq 1 ]; 
-then 
-    input=$1
-    short=$( echo $input | tr -d ' ' )
-    resultat=""
-    somme=0
-    if [[ $num =~ ^-?[0-9]+$ ]] || [ ${#input} -le 1 ]; 
-    then
-        echo "false"
-        exit 0
-    else
-        for (( i=1; i<=${#short}; i++ ))
-        do
-            chiffre=$(echo $short[i] | cut -c$i)
-            if [ $((i%2)) -eq 1 ]; 
-            then
-                double=$((chiffre*2))
-                if [ $double -gt 9 ]; 
-                then
-                    doubleGrand=$((double-9))
-                    resultat+="$doubleGrand"
-                else
-                    resultat+="$double"
-                fi
-            else
-                resultat+="$chiffre"
-            fi
-        done
-        for (( i=1; i<=${#short}; i++ ))
-        do
-            chiffre=$(echo $resultat[i] | cut -c$i)
-            somme=$((somme+chiffre))
-        done
-        echo $resultat
-        if [ $((somme%10)) -eq 0 ];
-        then
-            echo "true"
-        else
-            echo "false"
+if [[ $input =~ [^0-9[:space:]] ]]; then
+    echo false
+    exit 0
+fi
+
+input=$(echo "$input" | tr -d ' ' | rev)
+
+sum=0
+for (( i=0; i<${#input}; i++ )); do
+    digit=${input:i:1}
+
+    if [ $((i%2)) -eq 1 ]; then
+        digit=$((digit * 2))
+        if [ $digit -gt 9 ]; then
+            digit=$((digit - 9))
         fi
     fi
+
+    sum=$((sum + digit))
+done
+
+if [ $((sum % 10)) -eq 0 ] && [ ${#input} -gt 1 ]; then
+    echo true
 else
-    echo "Il y a plus de deux paramètres"
-    exit 1
+    echo false
 fi
