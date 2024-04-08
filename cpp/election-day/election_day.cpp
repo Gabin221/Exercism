78d7c281e6a0ba2a7c9ec90331180b7c24dc77a0
Gabin221 <serrurot19@gmail.com>
2024-04-08 13:57:00 +0200

    Update election_day.cpp

--- cpp/election-day/election_day.cpp
+++ cpp/election-day/election_day.cpp
@@ -12,6 +12,28 @@ struct ElectionResult {
     int votes{};
 };
 
+int vote_count(ElectionResult name) {
+    return name.votes;
+}
+
+void increment_vote_count(ElectionResult & name, int count) {
+    name.votes += count;
+}
+
+std::string determine_result(std::vector<ElectionResult> final_count) {
+    std::string result = "President ";
+    std::string nom = "";
+    int max_count = 0;
+    for (int i = 0; i < final_count.size(); i++) {
+        if (final_count[i].votes > max_count) {
+            max_count = final_count[i].votes;
+            nom = final_count[i].name;
+        }
+    }
+
+    return result + nom;
+}
+
 // TODO: Task 1
 // vote_count takes a reference to an `ElectionResult` as an argument and will
 // return the number of votes in the `ElectionResult.
