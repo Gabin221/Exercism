340972f0e1448127bf1209b16f3b18b3b3afb50d
Gabin221 <serrurot19@gmail.com>
2024-04-09 17:28:44 +0200

    Update

--- cpp/troll-the-trolls/CMakeFiles/test_troll-the-trolls.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_troll-the-trolls"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_troll-the-trolls.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
