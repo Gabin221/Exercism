0997a14239baa69fe00a923ed23824fec8aeb8d7
Gabin221 <serrurot19@gmail.com>
2024-04-10 16:52:45 +0200

    Update

--- bash/atbash-cipher/atbash_cipher.sh
+++ bash/atbash-cipher/atbash_cipher.sh
@@ -1,24 +1,62 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+nbr=$#
+
+if [ $nbr -eq 2 ]; 
+then 
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    nombres="0123456789"
+    cipher="zyxwvutsrqponmlkjihgfedcba"
+    input=$(tr '[:upper:]' '[:lower:]' <<< "${2// }")
+    intermediaire=""
+    reponse=""
+
+    if [ "$1" = "encode" ];
+    then
+        for (( i=1; i<=${#input}; i++ ))
+        do
+            recherche="$(echo $input[i] | cut -c$i)"
+            if [[ $alphabet == *"$recherche"* ]];
+            then 
+                debut_chaine="${alphabet%%${recherche}*}"
+                position=$((${#debut_chaine}+1))
+                intermediaire+="$(echo $cipher[position] | cut -c$position)"
+            elif [[ $nombres == *"$recherche"* ]];
+            then 
+                intermediaire+="$recherche"
+            fi
+        done
+
+        for (( i=1; i<=${#intermediaire}; i++ ))
+        do
+            chaine="$(echo $intermediaire[i] | cut -c$i)"
+            if [ $((i%5)) -eq 0 ]; 
+            then 
+                reponse+="$chaine "
+            else
+                reponse+="$(echo $intermediaire[i] | cut -c$i)"
+            fi
+        done
+
+        echo $reponse
+    else
+        for (( i=1; i<=${#input}; i++ ))
+        do
+            recherche="$(echo $input[i] | cut -c$i)"
+            if [[ $cipher == *"$recherche"* ]];
+            then 
+                debut_chaine="${cipher%%${recherche}*}"
+                position=$((${#debut_chaine}+1))
+                reponse+="$(echo $alphabet[position] | cut -c$position)"
+            elif [[ $nombres == *"$recherche"* ]];
+            then 
+                reponse+="$recherche"
+            fi
+        done
+
+        echo $reponse
+    fi
+else 
+    echo "Usage: atbash-cipher.sh <string2>"
+    exit 1
+fi
