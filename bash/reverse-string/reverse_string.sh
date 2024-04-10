8a089d07046d29a24cea813189b6ca09fbb29a40
Gabin221 <serrurot19@gmail.com>
2024-04-10 19:27:17 +0200

    Update reverse_string.sh

--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+nbr=$#
+if [ $nbr -eq 1 ]; 
+then 
+    input=$1
+
+    for (( i=1; i<=${#input}; i++ ))
+    do
+        echo $(echo $input[i] | cut -c$i)
+    done
+else
+    echo "Usage: reverse_string.sh <string2>"
+    exit 1
+fi
