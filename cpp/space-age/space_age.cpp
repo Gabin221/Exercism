24945229de9d71a5297dfe5c4f1c7af7ad6c358b
Gabin221 <serrurot19@gmail.com>
2024-04-30 19:34:32 +0200

    Update

--- cpp/space-age/space_age.cpp
+++ cpp/space-age/space_age.cpp
@@ -1,5 +1,7 @@
 #include "space_age.h"
-
 namespace space_age {
-
-}  // namespace space_age
+  space_age::space_age(long seconds) : n(seconds) {}
+  long space_age::seconds() const {
+    return n;
+  }
+}
