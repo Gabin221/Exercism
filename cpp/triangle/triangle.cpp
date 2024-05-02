743d2c5a3fb71043152783b87dab775aa6df51d5
Gabin221 <serrurot19@gmail.com>
2024-05-02 14:03:07 +0200

    Update

--- cpp/triangle/triangle.cpp
+++ cpp/triangle/triangle.cpp
@@ -1,5 +1,25 @@
 #include "triangle.h"
+#include <iostream>
 
 namespace triangle {
 
+    flavor kind(float a, float b, float c) {
+        if (a + b >= c && b + c >= a && a + c >= b) {
+            if (a == b && a == c) {
+                return flavor::equilateral;
+            } else if (a ==b || a == c || b == c) {
+                return flavor::isosceles;
+            } else {
+                return flavor::scalene;
+            }
+        } else {
+            
+        }
+    }
+
 }  // namespace triangle
+
+int main() {
+    std::cout << triangle::kind(2, 2, 2) << std::endl;
+    return 0;
+}
--- cpp/triangle/triangle.h
+++ cpp/triangle/triangle.h
@@ -3,6 +3,14 @@
 
 namespace triangle {
 
+    enum class flavor {
+        equilateral,
+        isosceles,
+        scalene
+    };
+
+    flavor kind(float a, float b, float c);
+
 }  // namespace triangle
 
 #endif // TRIANGLE_H
