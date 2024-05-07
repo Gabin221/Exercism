9b17abafd68618ef6bb9fdcc14e6b63bf9a211d9
Gabin221 <serrurot19@gmail.com>
2024-05-07 17:46:45 +0200

    Update

--- cpp/grade-school/grade_school.cpp
+++ cpp/grade-school/grade_school.cpp
@@ -2,4 +2,14 @@
 
 namespace grade_school {
 
+    void school::add(std::string student, int grade) {
+        if (rost.find(grade) == rost.end()) {
+            rost.insert({grade, std::vector<std::string>(1, student)});
+        }
+        else {
+            rost.at(grade).push_back(student);
+        }
+        sort(rost.at(grade).begin(), rost.at(grade).end());
+    }
+
 }  // namespace grade_school
--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,18 +1,26 @@
 #if !defined(GRADE_SCHOOL_H)
 #define GRADE_SCHOOL_H
 
-#include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
 
 namespace grade_school {
 
-    // class school{
-    //     private:
-    //         std::string name;
-    //         int grade;
-    //     public:
+    class school{
+        private:
+            std::map<int, std::vector<std::string>> rost;
+        public:
+            void add(std::string student, int grade);
+            std::map<int, std::vector<std::string>> roster() const { 
+                return rost; 
+            };
+            std::vector<std::string> grade(int g) const { 
+                return rost.find(g) != rost.end() ? rost.at(g) : std::vector<std::string>(); 
+            };
             
-    // };
+    };
 
     
 
