8b98903834eb025fca9655d140e47f4950a7bff7
Gabin221 <serrurot19@gmail.com>
2024-05-07 15:59:02 +0200

    Update

--- cpp/grains/grains.cpp
+++ cpp/grains/grains.cpp
@@ -1,5 +1,21 @@
 #include "grains.h"
+#include <math.h>
 
 namespace grains {
+
+    unsigned long int square(int n) {
+        unsigned long int res = pow(2, n-1);
+        return res;
+    }
+
+    unsigned long int total() {
+        unsigned long int nbr = 0;
+
+        for (int i = 1; i <= 64; i++) {
+            nbr += square(i);
+        }
+
+        return nbr;
+    }
 
 }  // namespace grains
--- cpp/grains/grains.h
+++ cpp/grains/grains.h
@@ -3,6 +3,9 @@
 
 namespace grains {
 
+    unsigned long int square(int nbr);
+    unsigned long int total();
+
 }  // namespace grains
 
 #endif // GRAINS_H
