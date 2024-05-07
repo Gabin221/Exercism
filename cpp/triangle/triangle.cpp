f52e6c58c2dbc732b7b353cfd45aacbc513c997d
Gabin221 <serrurot19@gmail.com>
2024-05-07 15:29:09 +0200

    Update

--- cpp/triangle/triangle.cpp
+++ cpp/triangle/triangle.cpp
@@ -4,7 +4,7 @@
 namespace triangle {
 
     flavor kind(double a, double b, double c) {
-        if ((a > 0 && b > 0 && c > 0) || (a + b >= c && b + c >= a && a + c >= b)) {
+        if ((a > 0 && b > 0 && c > 0) && (a + b >= c && b + c >= a && a + c >= b)) {
             if (a == b && a == c) {
                 return flavor::equilateral;
             } else if (a == b || a == c || b == c) {
