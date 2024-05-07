868bdc532add9a448d1e321c1a3aedf1ab980749
Gabin221 <serrurot19@gmail.com>
2024-05-07 16:49:39 +0200

    Update

--- cpp/pangram/pangram.cpp
+++ cpp/pangram/pangram.cpp
@@ -2,4 +2,34 @@
 
 namespace pangram {
 
+    bool verif(std::string str, std::string substr){
+        if (str.find(substr) != std::string::npos) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool is_pangram(std::string sentence) {
+        std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        std::string lettresPassees = "";
+
+        std::transform(sentence.begin(), sentence.end(), sentence.begin(), [](unsigned char c){ return std::tolower(c); });
+
+        std::string result = sentence.substr(0, sentence.length());
+
+        for (long unsigned int i = 0; i < sentence.size(); ++i) {
+            std::string character = sentence.substr(i, 1);
+            if (verif(alphabet, character) && !verif(lettresPassees, character)) {
+                lettresPassees += character;
+            }
+        }
+
+        if (lettresPassees.size() == 26) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }  // namespace pangram
--- cpp/pangram/pangram.h
+++ cpp/pangram/pangram.h
@@ -1,7 +1,12 @@
 #if !defined(PANGRAM_H)
 #define PANGRAM_H
 
+#include <algorithm>
+#include <string>
+
 namespace pangram {
+
+    bool is_pangram(std::string sentence);
 
 }  // namespace pangram
 
