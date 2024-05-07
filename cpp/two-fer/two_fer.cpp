2c1a95f1190494a118270c1adbafb83aa86ff931
Gabin221 <serrurot19@gmail.com>
2024-05-07 16:59:42 +0200

    Update

--- cpp/two-fer/two_fer.cpp
+++ cpp/two-fer/two_fer.cpp
@@ -2,6 +2,12 @@
 
 namespace two_fer
 {
+    std::string two_fer(){
+        return "One for you, one for me.";
+    }
+
+    std::string two_fer(std::string name){
+        return "One for " + name + ", one for me.";
+    }
 
 } // namespace two_fer
-
