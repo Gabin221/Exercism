016a367e40204f46b877e24541a81b251fb81b7e
Gabin221 <serrurot19@gmail.com>
2024-05-07 17:17:02 +0200

    Update grade_school.h

--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,7 +1,20 @@
 #if !defined(GRADE_SCHOOL_H)
 #define GRADE_SCHOOL_H
 
+#include <iostream>
+#include <string>
+
 namespace grade_school {
+
+    // class school{
+    //     private:
+    //         std::string name;
+    //         int grade;
+    //     public:
+            
+    // };
+
+    
 
 }  // namespace grade_school
 
