53665775cf2d4c9d5eea05f03acb2d66cc6442ce
Gabin221 <serrurot19@gmail.com>
2024-09-25 21:11:41 +0200

    Update

--- powershell/bob/BobResponse.ps1
+++ powershell/bob/BobResponse.ps1
@@ -1,32 +1,93 @@
 Function Get-BobResponse() {
-    <#
-    .SYNOPSIS
-    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-    
-    .DESCRIPTION
-    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 
-    Bob answers 'Sure.' if you ask him a question.
-
-    He answers 'Whoa, chill out!' if you yell at him.
-
-    He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-
-    He says 'Fine. Be that way!' if you address him without actually saying
-    anything.
-
-    He answers 'Whatever.' to anything else.
-    
-    .PARAMETER HeyBob
-    The sentence you say to Bob.
-    
-    .EXAMPLE
-    Get-BobResponse -HeyBob "Hi Bob"
-    #>
     [CmdletBinding()]
     Param(
         [string]$HeyBob
     )
+    
+    If ((IsSilence($HeyBob)))
+    {
+    return ("Fine. Be that way!")
+    }
 
-    Throw "Please implement this function"
+    If ((IsQuestion($HeyBob)) -and (IsUpperCase($HeyBob)) -and (HasLetter($HeyBob)))
+    {
+    return ("Calm down, I know what I'm doing!")
+    }
+
+    If (IsQuestion($HeyBob))
+    {
+    return ('Sure.')
+    }
+
+    If ((IsUpperCase($HeyBob)) -and (HasLetter($HeyBob)))
+    {
+    return('Whoa, chill out!')
+    }
+
+    return("Whatever.")
+}
+
+Function IsQuestion() {
+    Param(
+        [string]$Text
+    )
+    $Text=OnlyText($Text)
+    If ($Text[$Text.Length - 1] -eq "?")
+    {
+    return ($true)
+    }
+}
+
+Function IsUpperCase() {
+    Param(
+        [string]$Text
+    )
+    $Text=OnlyText($Text)
+    If ($Text.ToUpper() -ceq $Text)
+    {
+    return($true)
+    }
+}
+Function OnlyText() {
+    Param(
+        [string]$Text
+    )
+    return ($Text.Trim())
+}
+
+Function IsSilence() {
+    Param(
+        [string]$Text
+    )
+    If (OnlyText($Text) -ne "")
+    {
+    return ($false)
+    }
+    else 
+    {
+    return ($true)
+    }
+    
+}
+
+Function HasLetter() {
+    Param(
+        [string]$Text   
+    )
+    
+    $Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        
+    while ($val -ne $Text.Length) 
+    {
+        $val++
+        
+        if ($Letters.Contains($Text[$val -1 ])){
+            return $true
+        }
+        
+        
+    }
+    
+    return $false
 }
