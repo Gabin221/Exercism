d80ab661a03b47bc4ea5080f23f483fb31d2cef5
Gabin221 <serrurot19@gmail.com>
2024-09-27 19:19:34 +0200

    Update

---
+++ powershell/strain/Strain.ps1
@@ -0,0 +1,17 @@
+Function Invoke-Keep() {
+    [CmdletBinding()]
+    Param(
+        [Object[]]$Data,
+        [ScriptBlock]$Predicate
+    )
+    Throw "Please implement this function"
+}
+
+Function Invoke-Discard() {
+    [CmdletBinding()]
+    Param(
+        [Object[]]$Data,
+        [ScriptBlock]$Predicate
+    )
+    Throw "Please implement this function"
+}
