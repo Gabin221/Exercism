95ae0cf6344bb234899bd98110ec54d84bacfa94
Gabin221 <serrurot19@gmail.com>
2024-09-28 19:06:43 +0200

    Update

--- powershell/resistor-color/ResistorColor.ps1
+++ powershell/resistor-color/ResistorColor.ps1
@@ -1,36 +1,22 @@
 Function Get-ColorCode() {
-    <#
-    .SYNOPSIS
-    Translate a color to its corresponding color code.
-
-    .DESCRIPTION
-    Given a color, return its corresponding color code.
-
-    .PARAMETER Color
-    The color to translate to its corresponding color code.
-
-    .EXAMPLE
-    Get-ColorCode -Color "black"
-    #>
     [CmdletBinding()]
     Param(
         [string]$Color
     )
-    
-    throw "Please implement this function"
+
+    $Colors = Get-Colors
+    $Compteur = 0
+
+    while ($Colors[$Compteur] -ne $Color) {
+        $Compteur++
+    }
+
+    return $Compteur
 }
 
 Function Get-Colors() {
-    <#
-    .SYNOPSIS
-    Return the list of all colors.
 
-    .DESCRIPTION
-    Return the list of all colors.
-
-    .EXAMPLE
-    Get-Colors
-    #>
+    $List = @("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
     
-    throw "Please implement this function"
+    return $List
 }
