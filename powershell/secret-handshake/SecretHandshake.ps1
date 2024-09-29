387b4326ae4e0dd79c0388db16ebeee129bda02e
Gabin221 <serrurot19@gmail.com>
2024-09-29 17:01:06 +0200

    Update

--- powershell/secret-handshake/SecretHandshake.ps1
+++ powershell/secret-handshake/SecretHandshake.ps1
@@ -1,29 +1,58 @@
-Function Invoke-SecretHandshake() {
-    <#
-    .SYNOPSIS
-    Convert a number between 1 and 31 to a sequence of actions in the secret handshake.
-
-    .DESCRIPTION
-    The sequence of actions is chosen by looking at the rightmost five digits of the number once it's been converted to binary.
-    Start at the right-most digit and move left.
-
-    The actions for each number place are:
-    00001 = wink
-    00010 = double blink
-    00100 = close your eyes
-    01000 = jump
-    10000 = Reverse the order of the operations in the secret handshake.
+function Get-Binary() {
+    param (
+        $Num
+    )
     
-    .PARAMETER Number
-    The value to be converted into a sequence of actions.
+    $bin = @{}
+    $bit = 8
+    while ($bit -ge 0) {
+        $tmp = ($Num % 2)
+        $bin[$bit] = $tmp
+        $Num = [math]::truncate($Num / 2)
+        $bit--
+    }
 
-    .EXAMPLE
-    Invoke-SecretHandshake -Number 2
-    Returns: @("double blink")
-     #>
+    return $bin.Values
+}
+
+Function Invoke-SecretHandshake() {
     [CmdletBinding()]
     Param(
         [int]$Number
     )
-    Throw "Please implement this function"
+
+    $Bin = Get-Binary -Num $Number
+    $Sequences = @()
+
+    for ($i = 0; $i -lt 4; $i++) {
+        switch ($i) {
+            0 { 
+                if ($Bin[$i] -eq 1) {
+                    $Sequences += "wink" 
+                }
+            }
+            1 { 
+                if ($Bin[$i] -eq 1) {
+                    $Sequences += "double blink" 
+                }
+            }
+            2 { 
+                if ($Bin[$i] -eq 1) {
+                    $Sequences += "close your eyes" 
+                }
+            }
+            3 { 
+                if ($Bin[$i] -eq 1) {
+                    $Sequences += "jump" 
+                }
+            }
+        }
+    }
+
+    if ($Bin[4] -eq 1) {
+        $ReverseSequences = $Sequences[($Sequences.count-1)..0]
+        return $ReverseSequences
+    }
+
+    return $Sequences
 }
