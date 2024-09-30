5fbaf33eaa29af267f504ed04e8d7c7f619179ce
Gabin221 <serrurot19@gmail.com>
2024-09-30 18:39:57 +0200

    Update

---
+++ powershell/hamming/HammingDifference.tests.ps1
@@ -0,0 +1,45 @@
+BeforeAll {
+    . "./HammingDifference.ps1"
+}
+
+Describe "Get-Hamming Test cases" {
+    Context "Passing Tests" {
+        It "empty strands" {
+            Get-HammingDifference '' '' | Should -Be 0
+        }
+        
+        It "single letter identical strands" {
+            Get-HammingDifference 'A' 'A' | Should -Be 0
+        }
+
+        It "single letter different strands" {
+            Get-HammingDifference 'G' 'T' | Should -Be 1
+        }
+
+        It "long identical strands" {
+            Get-HammingDifference "GGACTGAAATCTG" "GGACTGAAATCTG" | Should -Be 0
+        }
+
+        It "long different strands" {
+            Get-HammingDifference "GGACGGATTCTG" "AGGACGGATTCT" | Should -Be 9
+        }
+    }
+
+    Context "Invalid Inputs" {
+        It "disallow first strand longer" {
+            { Get-HammingDifference "AATG" "AAA" } | Should -Throw "*strands must be of equal length.*"
+        }
+
+        It "disallow second strand longer" {
+            { Get-HammingDifference "ATA" "AGTG" } | Should -Throw "*strands must be of equal length.*"
+        }
+
+        It "disallow left empty strand" {
+            { Get-HammingDifference "" "G" } | Should -Throw "*strands must be of equal length.*"
+        }
+
+        It "disallow right empty strand" {
+            { Get-HammingDifference "G" "" } | Should -Throw "*strands must be of equal length.*"
+        }
+    }
+}
--- powershell/nucleotide-count/NucleotideCount.ps1
+++ powershell/nucleotide-count/NucleotideCount.ps1
@@ -1,30 +1,48 @@
 Function Get-NucleotideCount() {
-    <#
-    .SYNOPSIS
-    Given a single stranded DNA string, compute how many times each nucleotide occurs in the string.
-    
-    .DESCRIPTION
-    The genetic language of every living thing on the planet is DNA.
-    DNA is a large molecule that is built from an extremely long sequence of individual elements called nucleotides.
-    4 types exist in DNA and these differ only slightly and can be represented as the following symbols: 'A' for adenine, 'C' for cytosine, 'G' for guanine, and 'T' thymine.
-
-    The function counts the occurances of A, C, G and T in the supplied strand.
-    And returns a hashtable in the format:
-
-    @{ A = 2; C = 2; G = 2; T = 3 }
-    
-    .PARAMETER Strand
-    The DNA strand to count
-    
-    .EXAMPLE
-    Get-NucleotideCount -Strand "ACGTAGCTT"
-    
-    Returns: @{ A = 2; C = 2; G = 2; T = 3 }
-    #>
     [CmdletBinding()]
     Param(
         [string]$Strand
     )
 
-    Throw "Please implement this function"
+    $Valide = $false
+    $Compteurs = @{ 
+        A = 0; 
+        C = 0; 
+        G = 0; 
+        T = 0 
+    }
+
+    for ($i = 0; $i -lt $Strand.length; $i++) {
+        if ($Strand[$i] -notlike "*A*" -and $Strand[$i] -notlike "*C*" -and $Strand[$i] -notlike "*G*" -and $Strand[$i] -notlike "*T*") {
+            $Valide = $false
+            break
+        } else {
+            $Valide = $true
+        }
+    }
+
+    if ($Valide -eq $true) {
+        for ($i = 0; $i -lt $Strand.length; $i++) {
+            switch ($Strand[$i]) {
+                'A' { 
+                    $Compteurs.A += 1
+                }
+                'C' { 
+                    $Compteurs.C += 1
+                }
+                'G' { 
+                    $Compteurs.G += 1
+                }
+                'T' {
+                    $Compteurs.T += 1
+                }
+            }
+        }
+
+        return $Compteurs
+    } elseif ($Strand -eq "") {
+        return $Compteurs
+    } else {
+        Throw "*Invalid nucleotide in strand*"
+    }
 }
